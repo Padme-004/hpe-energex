82ef9cfeba5e6d526bde80cb4b5b6bec6f066afb
padme-004 <shenoy.nidhi004@gmail.com>
2025-06-01 20:48:01 +0530

    NODE_ENV error fix (Deployemnt)

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds:  process.env.NODE_ENV === 'production', // Disables ESLint during builds
+    ignoreDuringBuilds:  true, // Disables ESLint during builds
   },
   typescript: {
     ignoreBuildErrors: true, // Optional: Disables TypeScript errors too
